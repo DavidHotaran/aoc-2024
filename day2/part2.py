3938727932f88c3b592a902d5b6e21ce16955b9d
David Hotaran <hotaran.d98@gmail.com>
2024-12-04 21:37:58 -0700

    day 2 done

---
+++ day2/part2.py
@@ -0,0 +1,50 @@
+with open("data.txt", "r") as file:
+    data: list[str] = file.readlines()
+
+total_safe = 0
+
+
+def is_increasing(nums):
+    prev = nums[0]
+    is_good = False
+    for num in nums[1:]:
+        if num > prev and abs(num - prev) >= 1 and abs(num - prev) <= 3:
+            is_good = True
+        else:
+            return False
+        prev = num
+    return is_good
+
+
+def is_decreasing(nums):
+    prev = nums[0]
+    is_good = False
+    for num in nums[1:]:
+        if num < prev and abs(num - prev) >= 1 and abs(num - prev) <= 3:
+            is_good = True
+        else:
+            return False
+        prev = num
+    return is_good
+
+
+for line in data:
+    report = [int(item) for item in line.strip().split(" ")]
+
+    if report[0] < report[1]:
+        if is_increasing(report):
+            total_safe += 1
+            continue
+    elif report[0] > report[1]:
+        if is_decreasing(report):
+            total_safe += 1
+            continue
+    for i in range(len(report)):
+        if is_decreasing(report[:i] + report[i + 1 :]) or is_increasing(
+            report[:i] + report[i + 1 :]
+        ):
+            total_safe += 1
+            break
+
+
+print(total_safe)
